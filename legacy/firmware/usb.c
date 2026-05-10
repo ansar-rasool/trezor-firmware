3a3597fa8555dc5496312c9ae4d5a93c32e6ac92
Ansar Rasool <ansarrasool123@gmail.com>
2026-05-10 11:54:47 +0800

    Name change
    Signed-off-by: Ansar Rasool <ansarrasool123@gmail.com>

--- legacy/firmware/usb.c
+++ legacy/firmware/usb.c
@@ -67,12 +67,12 @@
 #endif
 
 #define USB_STRINGS                                 \
-  X(MANUFACTURER, "SatoshiLabs")                    \
-  X(PRODUCT, "TREZOR")                              \
+  X(MANUFACTURER, "ORINOX")                    \
+  X(PRODUCT, "ORINOX")                              \
   X(SERIAL_NUMBER, config_uuid_str)                 \
-  X(INTERFACE_MAIN, "TREZOR Interface")             \
-  X(INTERFACE_DEBUG, "TREZOR Debug Link Interface") \
-  X(INTERFACE_U2F, "TREZOR U2F Interface")
+  X(INTERFACE_MAIN, "ORINOX Interface")             \
+  X(INTERFACE_DEBUG, "ORINOX Debug Link Interface") \
+  X(INTERFACE_U2F, "ORINOX U2F Interface")
 
 #define X(name, value) USB_STRING_##name,
 enum {
@@ -393,7 +393,7 @@ void usbInit(void) {
                        usbd_control_buffer, sizeof(usbd_control_buffer));
   usbd_register_set_config_callback(usbd_dev, set_config);
   usb21_setup(usbd_dev, &bos_descriptor);
-  static const char *origin_url = "trezor.io/start";
+  static const char *origin_url = "orinox.com";
   webusb_setup(usbd_dev, origin_url);
   // Debug link interface does not have WinUSB set;
   // if you really need debug link on windows, edit the descriptor in winusb.c
